d261b4fa6f823f62a836368b97f48637fd1e8e03
Christian Noel Reyes <darkcolonist@gmail.com>
2014-09-15 14:49:02 +0800

    added files from pastebin

--- symbolic links in windows.bat
+++
@@ -1,6 +0,0 @@
-C:\Users\admin\AppData\Roaming\Apple Computer\MobileSync>mklink /J "%APPDATA%\Ap
-ple Computer\MobileSync\Backup" "D:\iphone backups"
-Junction created for C:\Users\admin\AppData\Roaming\Apple Computer\MobileSync\Ba
-ckup <<===>> D:\iphone backups
- 
-C:\Users\admin\AppData\Roaming\Apple Computer\MobileSync>
