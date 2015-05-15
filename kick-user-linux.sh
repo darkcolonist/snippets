4482efb221f0c1609032840a22b901e018817f80
Christian Noel Reyes <darkcolonist@gmail.com>
2015-05-15 17:01:24 +0800

    Update kick-user-linux.sh
    using grep instead of cut because its more effective!

--- kick-user-linux.sh
+++ kick-user-linux.sh
@@ -3,10 +3,10 @@
 # @date: 2015-05-15
 # @usage: sudo bash kick.sh <username>
 # @usage: sudo bash kick.sh darkcolonist
-who | grep $1 | cut -d' ' -f2 | while read -r line ; do
+who | grep $1 | grep -o "pts/[0-9]*" | while read -r line ; do
   echo locating all processes of $line by $1;
   ps -dN | grep $line | cut -d' ' -f1 | while read -r line2 ; do
-    echo killing process line2;
+    echo killing process $line2;
     kill -9 $line2;
   done;
 done;
