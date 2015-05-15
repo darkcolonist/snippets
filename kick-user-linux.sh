efc5fd2211a497ef5fb5ddad5f56eae7010f7e68
Christian Noel Reyes <darkcolonist@gmail.com>
2015-05-15 17:25:26 +0800

    Update kick-user-linux.sh
    typo fix

--- kick-user-linux.sh
+++ kick-user-linux.sh
@@ -5,7 +5,7 @@
 # @usage: sudo bash kick.sh darkcolonist
 who | grep $1 | awk '{print $2}' | while read -r line ; do
   echo locating all processes of $line by $1;
-  ps -dN | grep pts/21 | awk '{print $1}' | while read -r line2 ; do
+  ps -dN | grep $line | awk '{print $1}' | while read -r line2 ; do
     echo killing process $line2;
     kill -9 $line2;
   done;
