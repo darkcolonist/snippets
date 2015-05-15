f8a1f8cff7a268a5382eee8e496fe4bbc27a978a
Christian Noel Reyes <darkcolonist@gmail.com>
2015-05-15 16:44:24 +0800

    Create kick-user-linux.sh

---
+++ kick-user-linux.sh
@@ -0,0 +1,12 @@
+# @author: Christian Noel Reyes <darkcolonist@gmail.com>
+# @description: kick user in linux (must be root)
+# @date: 2015-05-15
+# @usage: sudo bash kick.sh <username>
+# @usage: sudo bash kick.sh darkcolonist
+who | grep $1 | cut -d' ' -f2 | while read -r line ; do
+  echo locating all processes of $line by $1;
+  ps -dN | grep $line | cut -d' ' -f1 | while read -r line2 ; do
+    echo killing process line2;
+    kill -9 $line2;
+  done;
+done;
